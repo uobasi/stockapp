129dfebd30b45b0346712c85578b3afb75e8c43a
uobasi <54387747+uobasi@users.noreply.github.com>
2023-11-07 11:51:43 -0500

    Update GetAllTrades.py

--- GetAllTrades.py
+++ GetAllTrades.py
@@ -18,6 +18,7 @@ import json
 from polygon import exceptions
 from google.cloud.storage import Blob
 from google.cloud import storage
+from google.auth.exceptions import DefaultCredentialsError
 import requests
 
 year = str(date.today().year)
@@ -83,5 +84,5 @@ while True:
         blob.upload_from_string(json.dumps(final))
             
         
-    except(exceptions.BadResponse, requests.exceptions.ReadTimeout):
+    except(exceptions.BadResponse, requests.exceptions.ReadTimeout, DefaultCredentialsError):
         continue
