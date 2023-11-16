ba5346750b2098ec568d48be91a4dbc587a0f0e9
uobasi <54387747+uobasi@users.noreply.github.com>
2023-11-15 23:50:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -678,7 +678,7 @@ def plotChart(df, lst2, num1, num2, x_fake, df_dx, optionOrderList, stockName=''
         active=10,
         currentvalue={"prefix": "Price: "},
         pad={"t": 10},
-        steps=steps[17+trcount:]#[8::3]
+        steps=steps[19+trcount:]#[8::3]
     )]
 
     fig.update_layout(
