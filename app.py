973a09f62160b1d540ef330a90728cd158609a12
uobasi <54387747+uobasi@users.noreply.github.com>
2023-11-26 01:30:06 -0500

    .

--- app.py
+++ app.py
@@ -898,7 +898,7 @@ def splitHun(stkName, trad, quot, num1, num2, quodict):
 
 from dash import Dash, dcc, html, Input, Output, callback
 if stkName == 'TSLA':
-    inter = 150000
+    inter = 180000
     vpVal = 30
 else:
     inter = 60000
