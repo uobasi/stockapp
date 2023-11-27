012ad8567fcd3753e9d4dd1efc414e8814fef60b
uobasi <54387747+uobasi@users.noreply.github.com>
2023-11-27 09:39:39 -0500

    .

--- app.py
+++ app.py
@@ -923,13 +923,12 @@ app.layout = html.Div([
         interval=inter,
         n_intervals=0,
       )
-    html.Div(dcc.Input(id='input-on-submit', type='text')),
-    html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Div(id='container-button-basic',
-             children='Enter a value and press submit')
+    #html.Div(dcc.Input(id='input-on-submit', type='text')),
+    #html.Button('Submit', id='submit-val', n_clicks=0),
+    #html.Div(id='container-button-basic',children='Enter a value and press submit')
 
 ])
-
+'''
 @callback(
     Output('container-button-basic', 'children'),
     Input('submit-val', 'n_clicks'),
@@ -943,7 +942,7 @@ def update_output(n_clicks, value):
         return 'The input symbol was "{}" '.format(value)
     else:
         return 'The input symbol was "{}" is not accepted please try different symbol '.format(value)
-
+'''
 @callback(Output('graph', 'figure'),
           Input('interval', 'n_intervals'))
 
