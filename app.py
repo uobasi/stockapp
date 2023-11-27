2437c45ffac59e3d8e34ce495cb18fbe9024d06e
uobasi <54387747+uobasi@users.noreply.github.com>
2023-11-27 10:05:33 -0500

    .

--- app.py
+++ app.py
@@ -917,15 +917,16 @@ else:
     vpVal = 40
 app = Dash()
 app.layout = html.Div([
+    html.Div(dcc.Input(id='input-on-submit', type='text')),
+    html.Button('Submit', id='submit-val', n_clicks=0),
+    html.Div(id='container-button-basic',children='Enter a value and press submit'),
     dcc.Graph(id='graph'),
     dcc.Interval(
         id='interval',
         interval=inter,
         n_intervals=0,
       ),
-    html.Div(dcc.Input(id='input-on-submit', type='text')),
-    html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Div(id='container-button-basic',children='Enter a value and press submit')
+    
 
 ])
 
@@ -933,7 +934,7 @@ app.layout = html.Div([
     Output('container-button-basic', 'children'),
     Input('submit-val', 'n_clicks'),
     State('input-on-submit', 'value'),
-    prevent_initial_call=True
+    #prevent_initial_call=True
 )
 def update_output(n_clicks, value):
     value = str(value).upper() 
