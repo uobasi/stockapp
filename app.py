7b4c87f7d1ee279a46062b0e6ecfdc7430c84592
uobasi <54387747+uobasi@users.noreply.github.com>
2023-11-27 09:34:49 -0500

    .

--- app.py
+++ app.py
@@ -908,7 +908,7 @@ def splitHun(stkName, trad, quot, num1, num2, quodict):
 
 
 
-from dash import Dash, dcc, html, Input, Output, callback
+from dash import Dash, dcc, html, Input, Output, callback, State
 if stkName == 'TSLA':
     inter = 120000
     vpVal = 30
@@ -917,17 +917,16 @@ else:
     vpVal = 40
 app = Dash()
 app.layout = html.Div([
-    html.Div(dcc.Input(id='input-on-submit', type='text')),
-    html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Div(id='container-button-basic',
-             children='Enter a value and press submit')
     dcc.Graph(id='graph'),
     dcc.Interval(
         id='interval',
         interval=inter,
         n_intervals=0,
       )
-    
+    html.Div(dcc.Input(id='input-on-submit', type='text')),
+    html.Button('Submit', id='submit-val', n_clicks=0),
+    html.Div(id='container-button-basic',
+             children='Enter a value and press submit')
 
 ])
 
