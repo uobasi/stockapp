925a03f02c869dbe5519005c3c02dcb69e1fd974
uobasi <54387747+uobasi@users.noreply.github.com>
2023-11-27 09:27:39 -0500

    .

--- app.py
+++ app.py
@@ -917,16 +917,17 @@ else:
     vpVal = 40
 app = Dash()
 app.layout = html.Div([
+    html.Div(dcc.Input(id='input-on-submit', type='text')),
+    html.Button('Submit', id='submit-val', n_clicks=0),
+    html.Div(id='container-button-basic',
+             children='Enter a value and press submit')
     dcc.Graph(id='graph'),
     dcc.Interval(
         id='interval',
         interval=inter,
         n_intervals=0,
       )
-    html.Div(dcc.Input(id='input-on-submit', type='text')),
-    html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Div(id='container-button-basic',
-             children='Enter a value and press submit')
+    
 
 ])
 
