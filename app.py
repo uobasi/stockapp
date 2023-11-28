b41bc34fbf5a680d14d329819a6633ffdf0c5785
uobasi <54387747+uobasi@users.noreply.github.com>
2023-11-27 20:23:04 -0500

    .

--- app.py
+++ app.py
@@ -907,7 +907,7 @@ app.layout = html.Div([
 
     html.Div(dcc.Input(id='input-on-submit', type='text')),
     html.Button('Submit', id='submit-val', n_clicks=0),
-    #html.Div(id='container-button-basic',children='Enter a value and press submit'),
+    html.Div(id='container-button-basic',children='Enter a value and press submit'),
 
     dcc.Store(id='stkName-value')
     
@@ -917,6 +917,7 @@ app.layout = html.Div([
 
 @callback(
     Output('stkName-value', 'data'),
+    Output('container-button-basic', 'children'),
     Input('submit-val', 'n_clicks'),
     State('input-on-submit', 'value'),
     prevent_initial_call=True
