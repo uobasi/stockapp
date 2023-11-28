f7ddb362ea734bf5fc2268360a49e1c23e74ae41
uobasi <54387747+uobasi@users.noreply.github.com>
2023-11-27 20:04:49 -0500

    .

--- app.py
+++ app.py
@@ -907,7 +907,7 @@ app.layout = html.Div([
 
     html.Div(dcc.Input(id='input-on-submit', type='text')),
     html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Div(id='container-button-basic',children='Enter a value and press submit'),
+    #html.Div(id='container-button-basic',children='Enter a value and press submit'),
 
     dcc.Store(id='stkName-value')
     
