2ad8d9eddf1650bbfab988f418cba2013c4ad60d
uobasi <54387747+uobasi@users.noreply.github.com>
2023-12-10 16:56:17 -0500

    .

--- app.py
+++ app.py
@@ -217,8 +217,8 @@ def plotChart(df, lst2, num1, num2, x_fake, df_dx, optionOrderList, stockName=''
 
     pms = pd.Series([i[2] for i in OptionTimeFrame]).rolling(4).mean()
     cms = pd.Series([i[3] for i in OptionTimeFrame]).rolling(4).mean()
-    fig.add_trace(go.Scatter(x=pd.Series([i[0] for i in OptionTimeFrame]), y=pms, line=dict(color='red'), mode='lines', name='Put VMA'), row=2, col=1)
-    fig.add_trace(go.Scatter(x=pd.Series([i[0] for i in OptionTimeFrame]), y=cms, line=dict(color='green'), mode='lines', name='Call VMA'), row=2, col=1)
+    fig.add_trace(go.Scatter(x=pd.Series([i[0] for i in OptionTimeFrame]), y=pms, line=dict(color='red'), mode='lines', name='Put VMA'), row=2, col=2)
+    fig.add_trace(go.Scatter(x=pd.Series([i[0] for i in OptionTimeFrame]), y=cms, line=dict(color='green'), mode='lines', name='Call VMA'), row=2, col=2)
     
     #hovertext = []
     # for i in range(len(df)):
