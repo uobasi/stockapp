3b35b89650b3ba0f3d5d37792414b8849641010c
uobasi <54387747+uobasi@users.noreply.github.com>
2023-12-10 19:30:47 -0500

    .

--- app.py
+++ app.py
@@ -310,10 +310,11 @@ def plotChart(df, lst2, num1, num2, x_fake, df_dx, optionOrderList, stockName=''
                 # color="#ffffff"
             ),row=2, col=2)
     '''
-    fig.add_trace(go.Scatter(x=pd.Series([i[6] for i in mlst]), y=pd.Series([i[0] for i in mlst]), mode='markers',name='TradedPrice'), row=2, col=2)
     for ps in mlst:
         ps.append(df['time'].searchsorted(ps[6]))
-    fig.add_trace(go.Scatter(x=pd.Series([i[6] for i in mlst]), y=pd.Series([df['close'][i[7]] for i in mlst]), mode='lines',name='ClosingPrice'), row=2, col=2)
+    fig.add_trace(go.Scatter(x=pd.Series([i[6] for i in mlst if i[7] < len(df['close'])]), y=pd.Series([i[0] for i in mlst if i[7] < len(df['close'])]), mode='markers',name='TradedPrice'), row=2, col=2)
+    
+    fig.add_trace(go.Scatter(x=pd.Series([i[6] for i in mlst if i[7] < len(df['close'])]), y=pd.Series([df['close'][i[7]] for i in mlst if i[7] < len(df['close'])]), mode='lines',name='ClosingPrice'), row=2, col=2)
     
     #fig.add_trace(go.Scatter(x=pd.Series([df['time'][i[7]] for i in mlst]), y=pd.Series([i[0] for i in mlst]), mode='lines',name='TradedPrice'), row=1, col=1)
     
